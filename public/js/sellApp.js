d74c4f65d3da3670f07abe77eb9f2d2c8001336d
is2soccer <is2soccer@hotmail.com>
2017-02-09 05:10:40 -0500

    fixed that price input is not saved in database as number type, so changed type to text and only let user can type input [0-9] and also limited to 2nd decimal places

--- public/js/sellApp.js
+++ public/js/sellApp.js
@@ -25,6 +25,34 @@ angular.module("mainApp.sellApp", ['ngRoute'])
                 alert(response);
             });
         }
+    })
+    .directive('onlyDigits', function () {
+        return {
+          require: 'ngModel',
+          restrict: 'A',
+          link: function (scope, element, attr, ctrl) {
+            function inputValue(val) {
+              if (val) {
+                var digits = val.replace(/[^0-9.]/g, '');
+
+                if (digits.split('.').length > 2) {
+                  digits = digits.substring(0, digits.length - 1);
+                }
+                if (digits.split('.')[1].length > 2) {
+                    digits = digits.substring(0, digits.length -1);
+                }
+
+                if (digits !== val) {
+                  ctrl.$setViewValue(digits);
+                  ctrl.$render();
+                }
+                return parseFloat(digits);
+              }
+              return undefined;
+            }            
+            ctrl.$parsers.push(inputValue);
+          }
+        }
     });
 
     // this.createContact = function(contact) {
